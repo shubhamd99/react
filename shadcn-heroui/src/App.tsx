9863e51c13a74517b853d13613be19048f5d71e4
Shubham D <45649650+shubhamd99@users.noreply.github.com>
2026-03-17 23:47:53 +0530

    shadcn heroui project init

---
+++ shadcn-heroui/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-background">
+      <h1 className="text-4xl font-bold">Hello World</h1>
+    </div>
+  )
+}
+
+export default App
